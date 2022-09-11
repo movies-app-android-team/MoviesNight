637fe39e47d08a1c8473cc3771c2f248f2faac3e
Omar Muhammad <aztenz@outlook.com>
2022-09-11 15:06:08 +0200

    [add] test image

--- app/src/main/java/com/example/moviesnight/MainActivity.kt
+++ app/src/main/java/com/example/moviesnight/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.moviesnight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.moviesnight.slider.SlideItem
+import com.makeramen.roundedimageview.RoundedImageView
 
 class MainActivity : AppCompatActivity() {
-    private val viewPager
+    private lateinit var viewPager : ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewPager = findViewById(R.layout.item_view_slider)
+        viewPager = findViewById(R.id.imageSlider)
+        val items = mutableListOf<SlideItem>()
+        items.add(SlideItem(R.drawable.testimage))
     }
 }
