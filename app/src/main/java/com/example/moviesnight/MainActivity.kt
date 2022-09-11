66b4b1aa2d51a12ecf7f8c37e6546118af447f4f
Omar Muhammad <aztenz@outlook.com>
2022-09-11 16:28:48 +0200

    [add] navigation button actions

--- app/src/main/java/com/example/moviesnight/MainActivity.kt
+++ app/src/main/java/com/example/moviesnight/MainActivity.kt
@@ -1,38 +1,38 @@
 package com.example.moviesnight
 
-import android.content.ClipData
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
+import android.util.Log
 import com.example.moviesnight.fragment.AppNavigator
 import com.example.moviesnight.fragment.DetailFragment
 import com.example.moviesnight.fragment.HomeFragment
 import com.example.moviesnight.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity(),AppNavigator {
-    private lateinit var homeButton: MenuItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        supportFragmentManager
-//            .beginTransaction()
-//            .add(R.id.frameHomeContainer,HomeFragment())
-//            .add(R.id.frameDetailContainer,DetailFragment())
-//            .add(R.id.frameSearchContainer,SearchFragment())
-//            .commit()
+        val bubbleTB = findViewById<io.ak1.BubbleTabBar>(R.id.bubbleTabBar)
+        bubbleTB.addBubbleListener { id ->
+            Log.d("myApp", "button $id clicked")
+            when (id) {
+                R.id.navHome -> navigateToHome()
+                R.id.navSearch -> navigateToSearch()
+                R.id.navFav -> navigateToDetail()
+            }
+        }
     }
-
     override fun navigateToDetail() {
-        supportFragmentManager.beginTransaction().add(R.id.frameDetailContainer,DetailFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentHolder,DetailFragment()).commit()
     }
 
     override fun navigateToSearch() {
-        supportFragmentManager.beginTransaction().add(R.id.frameSearchContainer,SearchFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentHolder,SearchFragment()).commit()
     }
 
     override fun navigateToHome() {
-        supportFragmentManager.beginTransaction().add(R.id.frameHomeContainer,HomeFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentHolder,HomeFragment()).commit()
     }
 }
