84b15fe483abc361e2d250c278b45187a75d0f54
XXMariamX <rura.essam@hotmail.com>
2022-09-12 20:46:35 +0200

    [add] Search Fragment Adapter

--- app/src/main/java/com/example/moviesnight/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/moviesnight/fragment/SearchFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.moviesnight.R
+import com.example.moviesnight.recycler.GenreMovieAdapter
+import com.example.moviesnight.recycler.GenreMovieItem
 
 class SearchFragment : Fragment() {
-
+    private lateinit var searchResultRecycler: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view= inflater.inflate(R.layout.fragment_search, container, false)
+        searchResultRecycler=view.findViewById(R.id.searchResultsRecycler)
+
+        val searchMovies = mutableListOf<GenreMovieItem>()
+        searchMovies.add(GenreMovieItem("Good Father",1,2020,R.drawable.test2,4.2f))
+        searchMovies.add(GenreMovieItem("Hi",2,2020,R.drawable.test2,4.2f))
+        searchMovies.add(GenreMovieItem("Hello",3,2020,R.drawable.test2,4.2f))
+        searchMovies.add(GenreMovieItem("&&",20,2020,R.drawable.test2,10f))
+        searchMovies.add(GenreMovieItem(":-)",40,2020,R.drawable.test2,0f))
+
+        searchResultRecycler.adapter= GenreMovieAdapter(searchMovies)
+
+        return view
     }
 }
